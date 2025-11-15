9cfb7b43180a597935253ee976ab1749b70b2ee4
Pankaj <pankajsingh7875@gmail.com>
2025-11-15 12:30:04 +0530

    first jenkin script

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SimpleTest {
+
+	@Test
+    public void openGoogleTest() {
+        // WebDriverManager will download proper chromedriver at runtime
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.google.com");
+            String title = driver.getTitle();
+            Assert.assertTrue(title.toLowerCase().contains("google"));
+        } finally {
+            driver.quit();
+        }
+    }
+	
+}
